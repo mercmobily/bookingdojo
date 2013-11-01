1b2ab6f2f47c05865a7db793d41c94b837211285
Tony Mobily <tonymobily@gmail.com>
2013-11-01 23:24:40 +0800

    Indentation fixed

--- server.js
+++ server.js
@@ -49,45 +49,43 @@ var dbString = process.env.MONGO_URL || 'mongodb://localhost/hotplate';
   // (Do this after loading modules, which might set some defaults)
   configServer( app );
 
-  //hotplate.hotEvents.emit( 'setDefaults', function(){
-    //app.set('port', process.env.PORT || 3000);
-    app.set('port',  3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
+  //app.set('port', process.env.PORT || 3000);
+  app.set('port',  3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
 
-    // Various middleware
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
+  // Various middleware
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
 
-    app.use(passport.initialize()); // Passport initialize
-    app.use(express.cookieParser('woodchucks are nasty animals'));
+  app.use(passport.initialize()); // Passport initialize
+  app.use(express.cookieParser('woodchucks are nasty animals'));
 
-    // Static routes -- they MUST go before the session!
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use( hotplate.require('hotCoreClientFiles').serve() );
+  // Static routes -- they MUST go before the session!
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use( hotplate.require('hotCoreClientFiles').serve() );
 
-    app.use(express.cookieSession({
-      secret: 'woodchucks are nasty animal',
-    }));
+  app.use(express.cookieSession({
+    secret: 'woodchucks are nasty animal',
+  }));
 
-    hotplate.hotEvents.emit( 'setRoutes', app, function() { 
-      app.use( app.router);
-      app.use( hotplate.require('hotCoreError').hotCoreErrorHandler );
+  hotplate.hotEvents.emit( 'setRoutes', app, function() { 
+    app.use( app.router);
+    app.use( hotplate.require('hotCoreError').hotCoreErrorHandler );
 
-      app.use( function( err, req, res, next){ res.send("Oh dear, this should never happen!"); next(err); } );
+    app.use( function( err, req, res, next){ res.send("Oh dear, this should never happen!"); next(err); } );
 
-      hotplate.hotEvents.emit( 'run', function() { 
-        // Create the actual server
-        var server = http.createServer( app );
-        server.listen(app.get('port'), function(){
-          console.log("Express server listening on port " + app.get('port'));
-        });
-
+    hotplate.hotEvents.emit( 'run', function() { 
+      // Create the actual server
+      var server = http.createServer( app );
+      server.listen(app.get('port'), function(){
+        console.log("Express server listening on port " + app.get('port'));
       });
-    });     
-  // });
+
+    });
+  });     
 });
 
 
